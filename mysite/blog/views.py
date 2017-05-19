dfeb0b552a108c73773805d88001ba847abd9d0b
annie <you@example.com>
2017-05-20 03:24:50 +0900

    css만들기

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -2,10 +2,11 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
-
+from django.utils import timezone
 from .models import Post
 
 
 def post_list(request):
     post_all_data = Post.objects.all()
+    posts = Post.objects.filter(published_data__lte=timezone.now()).order_by('published_data')
     return render(request, 'post_list.html', {'posts': post_all_data})
