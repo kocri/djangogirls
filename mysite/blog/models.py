8da1b1184aed54be1735353d6776860961559c82
annie <you@example.com>
2017-05-20 15:44:15 +0900

    장고걸 폼

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -14,7 +14,7 @@ class Post(models.Model):
             blank=True, null=True)
 
     def publish(self):
-        self.published_date = timezone.now()
+        self.published_data = timezone.now()
         self.save()
 
     def __str__(self):
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -4,9 +4,33 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 from django.utils import timezone
 from .models import Post
+from django.shortcuts import render, get_object_or_404
+from .forms import PostForm
 
 
 def post_list(request):
     post_all_data = Post.objects.all()
     posts = Post.objects.filter(published_data__lte=timezone.now()).order_by('published_data')
     return render(request, 'post_list.html', {'posts': post_all_data})
+
+
+def post_detail(request, pk):
+        post = get_object_or_404(Post, pk=pk)
+        return render(request, 'blog/post_detail.html', {'post': post})
+
+def post_new(request):
+    form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
+
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_data = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
